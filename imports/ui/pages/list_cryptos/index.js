3f6fc916bd5a8f7de33e7e5c83c39b1d726827a6
Deleplanque Dylan <deleplanque@b13p22.fil.univ-lille1.fr>
2018-03-19 11:01:34 +0100

    fini

--- imports/ui/pages/list_cryptos/index.js
+++ imports/ui/pages/list_cryptos/index.js
@@ -5,5 +5,61 @@ import { Crypto } from '../../../api/crytocurrency/crytocurrency.js';
 import { Wallets } from '../../../api/wallets/wallets.js';
 
 // TODO : Insérer les souscriptions
+Template.list_crypto.onCreated(function() {
+  this.autorun(() => {
+    this.subscribe('crypto');
+  });
+});
 
 // TODO : Insérer le helper
+Template.list_crypto.helpers({
+  cryptos() {
+    return Crypto.find();
+  },
+});
+
+Template.list_crypto.onCreated(function() {
+  this.subscribe('Wallets');
+});
+
+Template.crypto.helpers({
+  inWallet() {
+    return Wallets.findOne({
+      $and: [{ code: this.code }, { owner: Meteor.userId() }],
+    }).nbCoins;
+  },
+});
+
+Template.navbar.onCreated(function() {
+  this.autorun(() => {
+    this.subscribe('Wallets');
+  });
+});
+
+Template.navbar.helpers({
+  dollarWallet() {
+    return (
+      Wallets.findOne({ $and: [{ owner: Meteor.userId() }, { code: 'usdt' }] })
+        .nbCoins | 0
+    );
+  },
+});
+
+Template.navbar.onCreated(function() {
+  this.autorun(() => {
+    this.subscribe('Wallets');
+    this.subscribe('Sales.owner');
+  });
+});
+
+Template.navbar.helpers({
+  dollarWallet() {
+    return (
+      Wallets.findOne({ $and: [{ owner: Meteor.userId() }, { code: 'usdt' }] })
+        .nbCoins | 0
+    );
+  },
+  salesCount() {
+    return Sales.find({ owner: Meteor.userId(), buyerId: '' }).count();
+  },
+});
